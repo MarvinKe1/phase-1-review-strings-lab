c842205966913583e0edb3cd605a97095eb8c290
Marvin Mango <marvinke2001@gmail.com>
2025-03-07 08:13:51 +0300

    Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage with 'Welcome to Flatbook, ' and the value of currentUser
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage with 'WELCOME TO FLATBOOK, ' and the uppercase value of currentUser
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting with the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
